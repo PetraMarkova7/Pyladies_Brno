dc1661b95fd8c5bfc8b24e61be2d6d78f1e6cfcb
Petra Marková <petra.markova@mergado.com>
2019-10-29 19:39:32 +0100

    zbytek

---
+++ 05/cislo.py
@@ -0,0 +1,5 @@
+rc = "0153273373"
+rc = rc.replace('/','')
+print("RČ:", rc)
+
+if jeRCspravne(rc):
---
+++ 05/test.py
@@ -0,0 +1,12 @@
+prvocisla = [2, 3, 5, 7, 11, 13, 17]
+print(prvocisla)
+prvocisla.append(19)
+print(prvocisla)
+
+dalsi_prvocisla = [23, 29, 31]
+prvocisla.extend(dalsi_prvocisla)
+print(prvocisla)
+seznam = []
+seznam.extend('abcdef')
+seznam.extend(range(10))
+print(seznam)
